190b974a01b094509f06f0a47f27fd44ab70a968
Koushik <venkatesh.nomula@microexcel.com>
2015-09-28 13:14:41 +0530

    added library to the project.....

--- platforms/android/src/com/performanceactive/plugins/camera/CustomCamera.java
+++ platforms/android/src/com/performanceactive/plugins/camera/CustomCamera.java
@@ -1,19 +1,20 @@
 package com.performanceactive.plugins.camera;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
+import static com.performanceactive.plugins.camera.CustomCameraActivity.FILENAME;
+import static com.performanceactive.plugins.camera.CustomCameraActivity.QUALITY;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONException;
 
-import static com.performanceactive.plugins.camera.CustomCameraActivity.ERROR_MESSAGE;
-import static com.performanceactive.plugins.camera.CustomCameraActivity.FILENAME;
-import static com.performanceactive.plugins.camera.CustomCameraActivity.IMAGE_URI;
-import static com.performanceactive.plugins.camera.CustomCameraActivity.QUALITY;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+
+import com.venkykowshik.squarecamera.CameraActivity;
 
 
 public class CustomCamera extends CordovaPlugin {
@@ -28,7 +29,7 @@ public class CustomCamera extends CordovaPlugin {
 	    }
 	    this.callbackContext = callbackContext;
 	    Context context = this.cordova.getActivity();
-	    Intent intent = new Intent(context, CustomCameraActivity.class);
+	    Intent intent = new Intent(context, CameraActivity.class);
 	    intent.putExtra(FILENAME, args.getString(0));
 	    intent.putExtra(QUALITY, args.getInt(1));
 	    cordova.startActivityForResult(this, intent, 0);
@@ -40,21 +41,34 @@ public class CustomCamera extends CordovaPlugin {
 	    return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
 	}
 
-	@Override
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-	    if (resultCode == Activity.RESULT_OK) {
-	        callbackContext.success(intent.getExtras().getString(IMAGE_URI));
-	    } else {
-	    	String errorMessage = null;
-	    	if (intent != null) {
-	    		 errorMessage = intent.getExtras().getString(ERROR_MESSAGE);
-			} 
-	        if (errorMessage != null) {
-	            callbackContext.error(errorMessage);
-	        } else {
-	            callbackContext.error("Failed to take picture");
-	        }
-	    }
-    }
+//	@Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+//	    if (resultCode == Activity.RESULT_OK) {
+//	        callbackContext.success(intent.getExtras().getString(IMAGE_URI));
+//	    } else {
+//	    	String errorMessage = null;
+//	    	if (intent != null) {
+//	    		 errorMessage = intent.getExtras().getString(ERROR_MESSAGE);
+//			} 
+//	        if (errorMessage != null) {
+//	            callbackContext.error(errorMessage);
+//	        } else {
+//	            callbackContext.error("Failed to take picture");
+//	        }
+//	    }
+//    }
 
+	 @Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	        if (resultCode != Activity.RESULT_OK) return;
+
+	        if (data.getExtras() != null) {
+	        	callbackContext.error("Failed to take picture");
+            } else {
+                Uri photoUri = data.getData();
+                callbackContext.success(photoUri.toString());
+            }
+
+	        super.onActivityResult(requestCode, resultCode, data);
+	    }
 }
